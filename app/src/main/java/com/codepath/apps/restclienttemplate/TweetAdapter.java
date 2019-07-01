b2be7cd91ce7cac779044d634fca01715e44cfe3
Sophia Zambri <sophiazambri@fb.com>
2019-07-01 16:20:55 -0700

    required story 1

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -0,0 +1,73 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
+
+    private List<Tweet> mTweets;
+
+    Context context;
+    // pass in Tweets array
+    public TweetAdapter(List<Tweet> tweets) {
+        mTweets = tweets;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View tweetView = inflater.inflate(R.layout.item_tweet, parent, false);
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+        return viewHolder;
+    }
+    // inflate the layout and cache references into ViewHolder
+
+    // bind the values on the position of the element
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        //get the data according to position
+        Tweet tweet = mTweets.get(position);
+        // populate the views according to this data
+        holder.tvUsername.setText(tweet.user.name);
+        holder.tvBody.setText(tweet.body);
+
+        Glide.with(context).load(tweet.user.profileImageUrl).into(holder.ivProfileImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+
+    // create ViewHolder class
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView ivProfileImage;
+        public TextView tvUsername;
+        public TextView tvBody;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            //perform findViewById lookups
+            ivProfileImage = (ImageView) itemView.findViewById(R.id.ivProfileImage);
+            tvUsername = (TextView) itemView.findViewById(R.id.tvUserName);
+            tvBody = (TextView) itemView.findViewById(R.id.tvBody);
+        }
+    }
+}
